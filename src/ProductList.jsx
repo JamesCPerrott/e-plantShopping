fee4043c6882cf6d42f0b558a1ea3b7cc5d326b4
James Perrott <62729193+JamesCPerrott@users.noreply.github.com>
2025-01-28 08:59:51 -0800

    Update ProductList.jsx
    name update

--- src/ProductList.jsx
+++ src/ProductList.jsx
@@ -288,11 +288,11 @@ function ProductList() {
              <div className="navbar" style={styleObj}>
             <div className="tag">
                <div className="luxury">
-               <img src="https://cdn.pixabay.com/photo/2020/08/05/13/12/eco-5465432_1280.png" alt="" />
+               <img src="https://cdn.pixabay.com/photo/2020/06/17/13/51/mountain-5309606_1280.jpg" alt="" />
                <a href="/" style={{textDecoration:'none'}}>
                         <div>
-                    <h3 style={{color:'white'}}>Paradise Nursery</h3>
-                    <i style={{color:'white'}}>Where Green Meets Serenity</i>
+                    <h3 style={{color:'white'}}>James' Mountain Greenery</h3>
+                    <i style={{color:'white'}}>Where Nature Meets the Peaks</i>
                     </div>
                     </a>
                 </div>
